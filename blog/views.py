e737eea946fccafcbf1a19787cca5e60689ab9be
enocol <eno.col@gmail.com>
2024-02-25 13:15:25 +0000

    Fix bug

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
 
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")    
     template_name = "post_list.html"
-
-
-# def post_data(request):
-#     template_name = "post.html"
-#     post = Post.objects.all()
-#     context = {"posts": post}
-#     return render(request, template_name, context)   
