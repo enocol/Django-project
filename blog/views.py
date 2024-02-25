362ad939fa93e3f7eeebf1a0a6aaff62c5b9d2d5
enocol <eno.col@gmail.com>
2024-02-25 12:29:08 +0000

    Add generic listView

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,12 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1).order_by("-created_on")    
     template_name = "post_list.html"
-    
+
+
+# def post_data(request):
+#     template_name = "post.html"
+#     post = Post.objects.all()
+#     context = {"posts": post}
+#     return render(request, template_name, context)   
