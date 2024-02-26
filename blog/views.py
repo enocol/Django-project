8c0f4e3c5218af12c34436a38b2cfaa127cb258c
enocol <eno.col@gmail.com>
2024-02-26 07:53:22 +0000

    Add base template and css

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post, Comment
+from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")    
-    template_name = "post_list.html"
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
--- codestar/settings.py
+++ codestar/settings.py
@@ -21,6 +21,7 @@ mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,6 +135,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEAIA_URL = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
