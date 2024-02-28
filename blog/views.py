80230da56227ae38966e2cda7db4fcafbf7dc2b6
enocol <eno.col@gmail.com>
2024-02-28 13:56:56 +0000

    Edit settings.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -12,17 +12,17 @@ class PostList(generic.ListView):
 
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post})
-
-
 # def post_detail(request, slug):
-#     queryset = Post.objects.filter(status=1)
-#     post = get_object_or_404(queryset, slug=slug)
+#     post = Post.objects.get(slug=slug)
+#     return render(request, "blog/post_detail.html", {"post": post})
 
-#     return render(
-#         request,
-#         "blog/post_detail.html",
-#         {"post": post},
-#     )
+
+def post_detail(request, slug):
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#4+auyz=wab#$(h&0c&sg1h=3#(qhfl5d8)nx3xlg6q%%cs54a'
+SECRET_KEY = os.environ.get("secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,13 +88,6 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
@@ -136,7 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEAIA_URL = 'media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
