53caf2eb16a38b16611dd03030da57523746365b
enocol <eno.col@gmail.com>
2024-03-02 14:39:27 +0000

    Add Django crispy forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.contrib import messages
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -19,11 +22,30 @@ class PostList(generic.ListView):
 def post_detail(request, id):
     queryset = Post.objects.all().order_by("-created_on")
     post = get_object_or_404(queryset, id=id)
+    comments = post.comments.filter(approved=True) 
+
+    
+    comment_form = CommentForm()
     context={"post": post,
-             "coder": "Enoh C",}
+             "coder": "Enoh C",
+             "comments": comments,
+             "comment_form": comment_form,}
+    
+    if request.method == "POST":
+       comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
 
     return render(
         request,
         "blog/post_detail.html",
         context
     )
+
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,4 @@
 console.log('Hello from script.js static file!')
+
+const h1 = document.getElementById('heading')
+console.log(h1)
